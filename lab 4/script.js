c93fb05fa70498d874b6fb3beb5f3f35f8311969
ratinder123503 <ratinderdeepKaur@dcmail.ca>
2025-03-14 14:54:25 -0400

    add story
    add

--- lab 4/script.js
+++ lab 4/script.js
@@ -8,3 +8,28 @@ function randomValueFromArray(array) {
 }
 
 const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+
+const insertX = ["Willy the Goblin",
+    "Big Daddy",
+    "Father Christmas"];
+    const insertY = ["the soup kitchen",
+    "Disneyland",
+    "the White House"];
+    const insertZ = ["spontaneously combusted",
+    "melted into a puddle on the sidewalk",
+    "turned into a slug and crawled away"];
+
+    randomize.addEventListener("click", result);
+    function result() {
+        let newStory = storyText;
+
+        const xItem = randomValueFromArray(insertX);
+        const yItem = randomValueFromArray(insertY);
+        const zItem = randomValueFromArray(insertZ);
+
+        newStory = newStory
+        .replace(":insertx:", xItem)
+        .replace(":insertx:", xItem)
+        .replace(":insertY:", yItem)
+        .replace(":insertz:", zItem);
+    }
