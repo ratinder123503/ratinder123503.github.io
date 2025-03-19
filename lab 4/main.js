c8f90adc56fca1a3138b933bd548aabf51647cee
ratinder123503 <ratinderdeepKaur@dcmail.ca>
2025-03-18 22:34:58 -0400

    add the js
    add

--- lab 4/main.js
+++ lab 4/main.js
@@ -32,5 +32,9 @@ btn.addEventListener('click', function () ) {
     const btnClass = btn.getAttribute('class');
     if (btnClass === 'dark') {
         btn.setAttribute('class', 'light');
+        btn.textContent = 'Lighten';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    } else {
+        btn.setAttribute('class', 'dark')
     }
 } 
