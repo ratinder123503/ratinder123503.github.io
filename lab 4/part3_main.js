6ca9a64e3614d0579d95f2d4b92303bd2cbd8fdf
ratinder123503 <ratinderdeepKaur@dcmail.ca>
2025-03-18 22:53:49 -0400

    add fuction randomRGB
    add

--- lab 4/part3_main.js
+++ lab 4/part3_main.js
@@ -11,3 +11,7 @@ const height = (canvas.height = window.innerHeight);
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }  
+
+function randomRGB() {
+    return `rgb(${random(0, 255)} ${random(0, 255)} ${random(0, 255)})`;
+  }
