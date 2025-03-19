1b78aa95bd40949ec001306cebd6b6cfe9402a7a
ratinder123503 <ratinderdeepKaur@dcmail.ca>
2025-03-18 22:28:00 -0400

    add alternative text, looping in js
    add

--- lab 4/main.js
+++ lab 4/main.js
@@ -5,15 +5,25 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const imagesFiles = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg']
+const imagesFiles = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
+const altTexts = [
+    'Closeup of a human eye',
+    'A rock sculpture',
+    'Purple flowers',
+    'Ancient wall painting',
+    'A butterfly on a leaf',
+];
 
 /* Looping through images */
-
+for (let i = 0; i <imagesFiles.length; i++) {
 const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+newImage.setAttribute('src', 'images/' = imagesFiles[i]);
+newImage.setAttribute('alt', altTexts[i]);
+newImage.addEventListener('click', function () {
 
+});
+thumbBar.appendChild(newImage);
+}
 /* Wiring up the Darken/Lighten button */  
